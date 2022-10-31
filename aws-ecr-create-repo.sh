7eff7c1dd7af024d83c4a51b63d764d7d93b306c
rohithzr <rohithzr@live.com>
2022-10-31 15:33:21 +0530

    fix condition else

--- aws-ecr-create-repo.sh
+++ aws-ecr-create-repo.sh
@@ -2,6 +2,6 @@
 
 aws ecr describe-repositories --repository-names $1 2>&1 > /dev/null
 status=$?
-if [[ ! "${status}" -eq 0 ]]; then
+if [ ! "${status}" -eq 0 ]; then
     aws ecr create-repository --repository-name $1
 fi

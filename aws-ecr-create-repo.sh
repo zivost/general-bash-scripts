4a50fb6368ba341abec93e6cc5b24b656ee431bd
rohithzr <rohithzr@live.com>
2022-10-31 12:59:55 +0530

    install aws cli script

---
+++ aws-ecr-create-repo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+aws ecr describe-repositories --repository-names $1 2>&1 > /dev/null
+status=$?
+if [[ ! "${status}" -eq 0 ]]; then
+    aws ecr create-repository --repository-name $1
+fi

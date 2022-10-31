3b9be9ec24256d193ff091f8dbae5834c1fa5559
rohithzr <rohithzr@live.com>
2022-10-31 12:59:34 +0530

    clean

--- ecr-create-repo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-aws ecr describe-repositories --repository-names $1 2>&1 > /dev/null
-status=$?
-if [[ ! "${status}" -eq 0 ]]; then
-    aws ecr create-repository --repository-name $1
-fi
